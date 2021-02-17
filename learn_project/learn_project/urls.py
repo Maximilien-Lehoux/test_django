6bb4faa79ff849dcd5f00a5cd0ffdeb489018670
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-02-17 09:30:08 +0100

    les pages basiques sont créées

--- learn_project/learn_project/urls.py
+++ learn_project/learn_project/urls.py
@@ -19,9 +19,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
+from store import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index),
+    # url(r'^store/', include('store.urls')),
+    url(r'^accounts/', include('accounts.urls')),
+    # url(r'^admin/', admin.site.urls)
 ]
+
 
 if settings.DEBUG:
     import debug_toolbar
